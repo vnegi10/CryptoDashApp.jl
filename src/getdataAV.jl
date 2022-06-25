71740e12664a9e840205e42339557cd4808d97f3
Vikas Negi <vikas.negi10@gmail.com>
2022-06-25 22:19:56 +0200

    Format getdata* functions

--- src/getdataAV.jl
+++ src/getdataAV.jl
@@ -1,7 +1,7 @@
 ################# Functions for AlphaVantage API #################
 
 function get_price_data_single(currency::String)
-    
+
     date = Dates.today()
 
     filename = "$(currency)_EUR_data_$(date).csv"
@@ -12,22 +12,22 @@ function get_price_data_single(currency::String)
     # Look for present day's CSV file, if not found, download and save data to a new file
     if isfile(filepath)
         @info "Reading $(currency) price/vol data from CSV file on disk"
-        raw_df = CSV.File(filepath) |> DataFrame        
+        raw_df = CSV.File(filepath) |> DataFrame
     else
-        try 
-            @info "Fetching $(currency) price/vol data from Alpha Vantage"  
-            raw = AlphaVantage.digital_currency_daily(currency, "EUR", datatype="csv") 
-            raw_df = raw_to_df(raw) 
-            CSV.write(filepath, raw_df)           
+        try
+            @info "Fetching $(currency) price/vol data from Alpha Vantage"
+            raw = AlphaVantage.digital_currency_daily(currency, "EUR", datatype = "csv")
+            raw_df = raw_to_df(raw)
+            CSV.write(filepath, raw_df)
         catch
-            @info "Could not fetch data, try again later!"          
-        end         
-    end 
-    
+            @info "Could not fetch data, try again later!"
+        end
+    end
+
     # Return processed DataFrame only when raw data has been fetched successfully
-    if ~isempty(raw_df)    
+    if ~isempty(raw_df)
         df_out_price, df_out_candle = average_price_df(currency, raw_df)
-        df_out_vol = vol_df(currency, raw_df)    
+        df_out_vol = vol_df(currency, raw_df)
 
         return df_out_price, df_out_candle, df_out_vol
     else
@@ -38,7 +38,7 @@ end
 function get_ratings_data(currency::String)
 
     date = Dates.today()
-    
+
     filename = "$(currency)_metrics_data_$(date).csv"
     filepath = joinpath("data", filename)
 
@@ -50,7 +50,7 @@ function get_ratings_data(currency::String)
         metrics_df = CSV.File(filepath) |> DataFrame
     else
         try
-            @info "Fetching $(currency) FCAS data from Alpha Vantage"  
+            @info "Fetching $(currency) FCAS data from Alpha Vantage"
             rating = AlphaVantage.crypto_rating(currency)
             scores = rating["Crypto Rating (FCAS)"]
             CSV.write(filepath, scores)
@@ -60,26 +60,28 @@ function get_ratings_data(currency::String)
                 @info "Could not retrieve data. Something wrong with the API, try again later!"
             else
                 @info "This is a new error: $(err)"
-            end            
-        end        
-    end        
+            end
+        end
+    end
 
     ratings = ["utility", "fcas score", "developer", "market", "fcas rating"]
-    index = Array{Int64}(undef,0)
+    index = Array{Int64}(undef, 0)
 
     # Return scores only when metrics data has been fetched successfully
     if ~isempty(metrics_df)
-        
+
         # Find the row(index) of a string match
         for rating in ratings
-            i = findall(occursin.(rating, metrics_df[!,1]))
+            i = findall(occursin.(rating, metrics_df[!, 1]))
             push!(index, i[1])
-        end    
-        
+        end
+
         # Variables should be assigned in the same order as the list of ratings above
-        utility_score, fcas_score, dev_score = metrics_df[!,2][index[1]], metrics_df[!,2][index[2]], metrics_df[!,2][index[3]]
-        mark_score, fcas_rating = metrics_df[!,2][index[4]], metrics_df[!,2][index[5]]
-        
+        utility_score, fcas_score, dev_score = metrics_df[!, 2][index[1]],
+        metrics_df[!, 2][index[2]],
+        metrics_df[!, 2][index[3]]
+        mark_score, fcas_rating = metrics_df[!, 2][index[4]], metrics_df[!, 2][index[5]]
+
         return utility_score, fcas_score, dev_score, mark_score, fcas_rating
     else
         return Number[], Number[], Number[], Number[], Number[]
--- src/getdataCG.jl
+++ src/getdataCG.jl
@@ -1,12 +1,12 @@
 ################# Functions for CoinGecko API #################
 
 function get_API_response(params::String, url::String = URL)
-	
-	CG_request = HTTP.request("GET", url * params; verbose = 0, retries = 2)
-	response_text = String(CG_request.body)
-	response_dict = JSON.parse(response_text)
-	
-	return response_dict
+
+    CG_request = HTTP.request("GET", url * params; verbose = 0, retries = 2)
+    response_text = String(CG_request.body)
+    response_dict = JSON.parse(response_text)
+
+    return response_dict
 end
 
 function get_coin_id(currency::String)
@@ -21,17 +21,17 @@ function get_coin_id(currency::String)
     # Look for present day's CSV file, if not found, download and save data to a new file
     if isfile(filepath)
         @info "Reading list of coins from CSV file on disk"
-        df_coins = CSV.File(filepath) |> DataFrame        
+        df_coins = CSV.File(filepath) |> DataFrame
     else
-        try 
-            @info "Fetching list of coins from CoinGecko"  
+        try
+            @info "Fetching list of coins from CoinGecko"
             coins_dict = get_API_response("/coins/list")
-            df_coins = vcat(DataFrame.(coins_dict)...) 
-            CSV.write(filepath, df_coins)           
+            df_coins = vcat(DataFrame.(coins_dict)...)
+            CSV.write(filepath, df_coins)
         catch
-            @info "Could not fetch data, try again later!"          
-        end         
-    end 
+            @info "Could not fetch data, try again later!"
+        end
+    end
 
     # Return valid coin id only when list of coins is available
     if ~isempty(df_coins)
@@ -44,17 +44,15 @@ function get_coin_id(currency::String)
             # which do not have "-" in them
             if size(df_filter)[1] > 1
 
-                df_filter_1 = df_filter |> 
-                            @filter(~occursin("-", _.id)) |> DataFrame
+                df_filter_1 = df_filter |> @filter(~occursin("-", _.id)) |> DataFrame
 
                 if isempty(df_filter_1)
-                    df_filter_1 = df_filter |> 
-                            @filter(~occursin("-", _.name)) |> DataFrame
+                    df_filter_1 = df_filter |> @filter(~occursin("-", _.name)) |> DataFrame
                 end
 
                 return df_filter_1[!, :id][1]
             end
-            
+
             return df_filter[!, :id][1]
 
         catch err
@@ -66,17 +64,17 @@ function get_coin_id(currency::String)
         end
 
     else
-        return ""   
+        return ""
     end
 end
 
 function dict_to_df(data_dict::Dict, df::DataFrame)
 
     # Collect only the key-value data which is suitable for plotting
-    for key in collect(keys(data_dict))            
+    for key in collect(keys(data_dict))
         if ~isnothing(data_dict[key]) && length(data_dict[key]) == 1
             push!(df, [key Float64(data_dict[key])])
-        end		
+        end
     end
 
     return df
@@ -89,16 +87,16 @@ function get_dev_comm_data(currency::String)
     coin_dict, dev_dict, comm_dict = [Dict() for i = 1:3]
 
     try
-        @info "Fetching coin data from CoinGecko" 
-        coin_dict = get_API_response("/coins/$(coin_id)")        
+        @info "Fetching coin data from CoinGecko"
+        coin_dict = get_API_response("/coins/$(coin_id)")
     catch
-        @info "Could not fetch data, try again later!" 
+        @info "Could not fetch data, try again later!"
     end
 
     # Get developer data
     try
         dev_dict = coin_dict["developer_data"]
-    catch err     
+    catch err
         if isa(err, KeyError)
             @info "Could not find developer data!"
         else
@@ -109,7 +107,7 @@ function get_dev_comm_data(currency::String)
     # Get community data
     try
         comm_dict = coin_dict["community_data"]
-    catch err     
+    catch err
         if isa(err, KeyError)
             @info "Could not find community data!"
         else
@@ -121,11 +119,11 @@ function get_dev_comm_data(currency::String)
     df_dev, df_comm = [DataFrame(Metric = String[], Value = Float64[]) for i = 1:2]
 
     if ~isempty(dev_dict)
-        df_dev = dict_to_df(dev_dict, df_dev)         
+        df_dev = dict_to_df(dev_dict, df_dev)
     end
 
-    if ~isempty(comm_dict) 
-        df_comm = dict_to_df(comm_dict, df_comm)        	
+    if ~isempty(comm_dict)
+        df_comm = dict_to_df(comm_dict, df_comm)
     end
 
     return df_dev, df_comm
@@ -143,28 +141,30 @@ function get_list_of_exchanges(num_exchanges::Int64)
     # Look for present day's CSV file, if not found, download and save data to a new file
     if isfile(filepath)
         @info "Reading list of exchanges from CSV file on disk"
-        df_ex = CSV.File(filepath) |> DataFrame        
+        df_ex = CSV.File(filepath) |> DataFrame
     else
-        try 
-            @info "Fetching list of exchanges from CoinGecko"  
+        try
+            @info "Fetching list of exchanges from CoinGecko"
             ex_dict = get_API_response("/exchanges")
-            df_all_ex = vcat(DataFrame.(ex_dict)...) 
+            df_all_ex = vcat(DataFrame.(ex_dict)...)
 
             # Keep only name and id columns
             df_ex = DataFrame(name = df_all_ex[!, :name], id = df_all_ex[!, :id])
-            CSV.write(filepath, df_ex)           
+            CSV.write(filepath, df_ex)
         catch err
             @info "Could not fetch data, try again later!"
-            @info "$(err)"          
-        end         
-    end 
+            @info "$(err)"
+        end
+    end
 
     # Return filtered list of exchanges only when data is available
     if ~isempty(df_ex)
-        df_ex_vol = DataFrame(Name = df_ex[!, :name][1:num_exchanges],
-                               ID  = df_ex[!, :id][1:num_exchanges])
+        df_ex_vol = DataFrame(
+            Name = df_ex[!, :name][1:num_exchanges],
+            ID = df_ex[!, :id][1:num_exchanges],
+        )
 
-        return df_ex_vol    
+        return df_ex_vol
     else
         return DataFrame()
     end
@@ -175,9 +175,9 @@ function get_exchange_vol_data(currency::String, num_exchanges::Int64)
     coin_id = get_coin_id(currency)
 
     df_ex_vol = get_list_of_exchanges(num_exchanges)
-    allowmissing!(df_ex_vol)    
+    allowmissing!(df_ex_vol)
 
-    exchange_coin_vol, exchange_usd_vol = [Union{Missing, Float64}[] for i = 1:2]
+    exchange_coin_vol, exchange_usd_vol = [Union{Missing,Float64}[] for i = 1:2]
 
     # Extract volume data only when a list of exchanges is available
     if ~isempty(df_ex_vol)
@@ -187,20 +187,24 @@ function get_exchange_vol_data(currency::String, num_exchanges::Int64)
             coin_vol, usd_vol = [Float64[] for i = 1:2]
             coin_vol_tickers_dict = Dict()
 
-            try 
-                coin_vol_tickers_dict = get_API_response("/exchanges/$(exchange)/tickers?coin_ids=$(coin_id)")
+            try
+                coin_vol_tickers_dict =
+                    get_API_response("/exchanges/$(exchange)/tickers?coin_ids=$(coin_id)")
             catch err
                 @info "Could not find $(coin_id) volume data on $(exchange)!"
                 @info "$(err)"
             end
-            
-            if ~isempty(coin_vol_tickers_dict)            
-            
-                for i = 1:length(coin_vol_tickers_dict["tickers"])			
+
+            if ~isempty(coin_vol_tickers_dict)
+
+                for i = 1:length(coin_vol_tickers_dict["tickers"])
                     push!(coin_vol, coin_vol_tickers_dict["tickers"][i]["volume"])
-                    push!(usd_vol, coin_vol_tickers_dict["tickers"][i]["converted_volume"]["usd"])                          
+                    push!(
+                        usd_vol,
+                        coin_vol_tickers_dict["tickers"][i]["converted_volume"]["usd"],
+                    )
                 end
-                
+
                 push!(exchange_coin_vol, sum(coin_vol))
                 push!(exchange_usd_vol, sum(usd_vol))
 
@@ -209,11 +213,16 @@ function get_exchange_vol_data(currency::String, num_exchanges::Int64)
                 push!(exchange_usd_vol, missing)
             end
 
-        end	
+        end
     end
 
-    insertcols!(df_ex_vol, 3, :Coin_volume => exchange_coin_vol, :USD_volume => exchange_usd_vol)
-    
+    insertcols!(
+        df_ex_vol,
+        3,
+        :Coin_volume => exchange_coin_vol,
+        :USD_volume => exchange_usd_vol,
+    )
+
     return df_ex_vol
 end
 
@@ -239,11 +248,12 @@ function get_vol_chart(exchange::String)
     else
         try
             # Fetch and save data for 365 days
-            ex_vol_chart = get_API_response("/exchanges/$(exchange)/volume_chart?days=$(days)") 
+            ex_vol_chart =
+                get_API_response("/exchanges/$(exchange)/volume_chart?days=$(days)")
 
             open(filepath, "w") do f
                 for i = 1:length(ex_vol_chart)
-                    writedlm(f, [ex_vol_chart[i]], ";")	
+                    writedlm(f, [ex_vol_chart[i]], ";")
                 end
             end
 
@@ -253,23 +263,23 @@ function get_vol_chart(exchange::String)
         end
     end
 
-    if length(ex_vol_chart) > days        
-        start_index = Int64(length(ex_vol_chart)/2) + 1        
+    if length(ex_vol_chart) > days
+        start_index = Int64(length(ex_vol_chart) / 2) + 1
         return ex_vol_chart[start_index:end]
 
     else
-        ex_vol = Union{Missing, Float64}[]
+        ex_vol = Union{Missing,Float64}[]
 
         for i = 1:length(ex_vol_chart)
             try
-                push!(ex_vol, round(parse(Float64, ex_vol_chart[i][2]); digits = 2))                        
+                push!(ex_vol, round(parse(Float64, ex_vol_chart[i][2]); digits = 2))
             catch
                 push!(ex_vol, missing)
-            end                    
-        end 
+            end
+        end
 
         return ex_vol
-    end    
+    end
 end
 
 function get_overall_vol_data(duration::Int64, num_exchanges::Int64)
@@ -283,12 +293,12 @@ function get_overall_vol_data(duration::Int64, num_exchanges::Int64)
 
     # Create a column with dates
     f_day = Dates.today()
-    i_day = f_day - Dates.Day(duration-1)
+    i_day = f_day - Dates.Day(duration - 1)
 
     time = collect(i_day:Dates.Day(1):f_day)
 
     df_ex_vol = DataFrame(Time = time)
-    allowmissing!(df_ex_vol) 
+    allowmissing!(df_ex_vol)
 
     # Extract volume data only when a list of exchanges is available
     if ~isempty(df_ex_list)
@@ -296,7 +306,7 @@ function get_overall_vol_data(duration::Int64, num_exchanges::Int64)
         for exchange in df_ex_list[!, :ID]
 
             ex_vol = Vector{Any}[]
-            
+
             try
                 ex_vol = get_vol_chart(exchange)
             catch err
@@ -305,20 +315,20 @@ function get_overall_vol_data(duration::Int64, num_exchanges::Int64)
 
                 # Skip next part of the code, and continue to next exchange
                 continue
-            end            
+            end
 
             # Check and filter on duration 
             if duration > length(ex_vol)
                 duration = length(ex_vol)
-            end           
-            ex_vol = ex_vol[end-duration+1:end]       
+            end
+            ex_vol = ex_vol[end-duration+1:end]
 
             # Find name of the exchange corresponding to its ID           
             df_row = df_ex_list |> @filter(_.ID == exchange) |> DataFrame
             name = df_row[!, :Name][1]
 
             try
-                insertcols!(df_ex_vol, 2, Symbol(name) => ex_vol)                
+                insertcols!(df_ex_vol, 2, Symbol(name) => ex_vol)
             catch err
                 if isa(err, DimensionMismatch)
                     @info "Data is missing for $(exchange) for the requested duration"
@@ -326,7 +336,7 @@ function get_overall_vol_data(duration::Int64, num_exchanges::Int64)
                     @info "Something went wrong, check this error: $(err)"
                 end
             end
-            
+
         end
 
         return df_ex_vol
@@ -334,4 +344,4 @@ function get_overall_vol_data(duration::Int64, num_exchanges::Int64)
         return DataFrame()
     end
 
-end    
+end
