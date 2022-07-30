de995c4d39c93cbbd3241feec5cfa87614010551
Vikas Negi <vikas.negi10@gmail.com>
2022-07-30 22:59:13 +0200

    Update tests

--- test/test_plots_AV.jl
+++ test/test_plots_AV.jl
@@ -2,11 +2,11 @@
 
 @testset "plot_price_ma_trade_data" begin
 
-    all_traces = CryptoDashApp.plot_price_ma_trade_data(7, 180, 30)
+    all_traces = CryptoDashApp.plot_price_ma_trade_data(7, 90, 30)
+    @test ~isempty(all_traces)
 
     for trace in all_traces
-        @test ~isempty(trace)
-        @test typeof(trace) == PlotlyBase.GenericTrace{Dict{Symbol, Any}}
+        @test trace.fields[:y] |> length == 90
     end   
 
 end
@@ -14,10 +14,10 @@ end
 @testset "plot_price_bollinger_bands" begin
 
     all_traces = CryptoDashApp.plot_price_bollinger_bands(13, 180, 30)
+    @test ~isempty(all_traces)
 
     for trace in all_traces
-        @test ~isempty(trace)
-        @test typeof(trace) == PlotlyBase.GenericTrace{Dict{Symbol, Any}}
+        @test trace.fields[:y] |> length == 180
     end   
 
 end
@@ -25,22 +25,32 @@ end
 @testset "plot_candle_vol_data" begin
 
     all_traces = CryptoDashApp.plot_candle_vol_data(18, 180)
+    @test ~isempty(all_traces)
 
-    for trace in all_traces
-        @test ~isempty(trace)
-        @test typeof(trace) == PlotlyBase.GenericTrace{Dict{Symbol, Any}}
-    end   
+    # Check candlestick data
+    candle_trace = all_traces[1]
+
+    for candle in [:high, :low, :open, :close]
+        @test candle_trace.fields[candle] |> length == 180
+    end
+
+    bar_trace = all_traces[2]
+    @test bar_trace.fields[:y] |> length == 180    
 
 end
 
 @testset "plot_cumul_daily_return_hist" begin
 
-    all_traces = CryptoDashApp.plot_cumul_daily_return_hist(12, 180)
+    all_traces = CryptoDashApp.plot_cumul_daily_return_hist(12, 60)
+    @test ~isempty(all_traces)
 
-    for trace in all_traces[1:4]
-        @test ~isempty(trace)
-        @test typeof(trace) == PlotlyBase.GenericTrace{Dict{Symbol, Any}}
-    end   
+    # Cumulative return
+    @test all_traces[1].fields[:y] |> length == 59
+
+    # Green and red returns will change with time, hence cannot be tested here
+
+    # Daily change histogram
+    @test all_traces[4].fields[:y] |> length == 78      
 
 end
 
@@ -56,22 +66,22 @@ end
 
 @testset "plot_macd_signal" begin
 
-    all_traces = CryptoDashApp.plot_macd_signal(2, 180)
+    all_traces = CryptoDashApp.plot_macd_signal(2, 42)
+    @test ~isempty(all_traces)
 
     for trace in all_traces
-        @test ~isempty(trace)
-        @test typeof(trace) == PlotlyBase.GenericTrace{Dict{Symbol, Any}}
-    end   
-
+        trace.fields[:y] |> length == 43
+    end
+    
 end
 
 @testset "plot_linear_regression" begin
 
-    all_traces = CryptoDashApp.plot_linear_regression(17, 180)
+    all_traces = CryptoDashApp.plot_linear_regression(17, 200)
+    @test ~isempty(all_traces)
 
     for trace in all_traces[1:4]
-        @test ~isempty(trace)
-        @test typeof(trace) == PlotlyBase.GenericTrace{Dict{Symbol, Any}}
+        @test trace.fields[:y] |> length == 200
     end   
 
 end
--- test/test_plots_CG.jl
+++ test/test_plots_CG.jl
@@ -3,32 +3,31 @@
 @testset "plot_dev_comm_data" begin
 
     all_traces = CryptoDashApp.plot_dev_comm_data(7)
+    @test ~isempty(all_traces)
 
-    for trace in all_traces
-        @test ~isempty(trace)
-        @test typeof(trace) == PlotlyBase.GenericTrace{Dict{Symbol, Any}}
-    end   
+    @test all_traces[1].fields[:y] |> length == 8
+    @test all_traces[2].fields[:y] |> length == 5 
 
 end
 
 @testset "plot_exchange_vol_data" begin
 
     all_traces = CryptoDashApp.plot_exchange_vol_data(7)
+    @test ~isempty(all_traces)
 
     for trace in all_traces
-        @test ~isempty(trace)
-        @test typeof(trace) == PlotlyBase.GenericTrace{Dict{Symbol, Any}}
-    end   
+        @test trace.fields[:y] |> length == 10
+    end    
 
 end
 
 @testset "plot_overall_vol_data" begin
 
     all_traces = CryptoDashApp.plot_overall_vol_data(30)
+    @test ~isempty(all_traces)
 
     for trace in all_traces
-        @test ~isempty(trace)
-        @test typeof(trace) == PlotlyBase.GenericTrace{Dict{Symbol, Any}}
+        @test trace.fields[:y] |> length == 30
     end   
 
 end
